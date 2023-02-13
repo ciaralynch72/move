e30814eb8f3d23d953d6cc1ba9e169350a6084e8
ciaralynch72 <Ciaralynch72@gmail.com>
2023-02-13 22:10:17 +0000

    Add category model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -12,6 +18,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -45,3 +52,5 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+
